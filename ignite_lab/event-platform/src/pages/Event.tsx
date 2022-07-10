5a77103811b5df668c6d47022107efdd2be3f19c
felipeferreiradev <felipeferreira.sp.dev@gmail.com>
2022-07-10 09:54:07 -0300

    Aula 03

--- ignite_lab/event-platform/src/pages/Event.tsx
+++ ignite_lab/event-platform/src/pages/Event.tsx
@@ -1,13 +1,30 @@
+import { RocketLaunch } from "phosphor-react";
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
+
+
+
 export function Event() {
+    const { slug } = useParams<{ slug: string }>();
+
     return(
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex flex-1">
-                <Video />
+                { slug 
+                    ? <Video lessonSlug={slug}/> 
+                    : (
+                        <div className="flex-1 items-center justify-center flex flex-col">
+                            <RocketLaunch size={128} color="#29292E"/>
+                            <strong className="mt-8 text-gray-400 text-sm">
+                                Selecione uma aula para assistir
+                            </strong>
+                        </div>
+                    )
+                }
                 <Sidebar />
             </main>
         </div>

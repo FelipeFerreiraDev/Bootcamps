857740481c7ebee939ecfb06d8cbc61eaad899f4
felipeferreiradev <felipeferreira.sp.dev@gmail.com>
2022-07-27 11:43:58 -0300

    Aula 02

--- ignite_lab/rockethelp/App.tsx
+++ ignite_lab/rockethelp/App.tsx
@@ -1,4 +1,4 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 import React from 'react';
 
@@ -6,6 +6,7 @@ import React from 'react';
 import { THEME } from './src/styles/theme';
 
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -15,7 +16,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
